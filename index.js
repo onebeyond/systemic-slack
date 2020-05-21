1576d1f9146616f06033149502d9de126c1cf12b
Jose V Giner <jose.giner@ara-tech.es>
2020-05-21 16:17:43 +0200

    refactor: change funcion name for upload

--- index.js
+++ index.js
@@ -3,45 +3,45 @@ const { createReadStream, existsSync } = require('fs');
 const { WebClient } = require('@slack/web-api');
 
 module.exports = () => {
-	let slackClient;
-	const start = async ({ config }) => {
-		const token = process.env.SLACK_TOKEN;
+  let slackClient;
+  const start = async ({ config }) => {
+    const token = process.env.SLACK_TOKEN;
 
-		if (!token) {
-			throw new Error('Slack API token is not set, please check configuration.');
-		}
+    if (!token) {
+      throw new Error('Slack API token is not set, please check configuration.');
+    }
 
-		slackClient = new WebClient(token);
+    slackClient = new WebClient(token);
 
-		const { iconEmoji, botName } = config;
+    const { iconEmoji, botName } = config;
 
-		const sendMessage = (text, room) => slackClient.chat.postMessage({
-			channel: room || process.env.SLACK_ROOM, text, icon_emoji: iconEmoji, username: botName,
-		});
+    const sendMessage = (text, room) => slackClient.chat.postMessage({
+      channel: room || process.env.SLACK_ROOM, text, icon_emoji: iconEmoji, username: botName,
+    });
 
-		const uploadFile = async (title, filepath, room) => {
-			debug(`Uploading ${filepath} to slack.`);
+    const sendFile = async (title, filepath, room) => {
+      debug(`Sending ${filepath} to slack.`);
 
-			if (!existsSync(filepath)) {
-				debug(`File ${filepath} not exists or i can't access it`);
-				throw new Error(`File ${filepath} not exists or i can't access it`);
-			}
+      if (!existsSync(filepath)) {
+        debug(`File ${filepath} not exists or i can't access it`);
+        throw new Error(`File ${filepath} not exists or i can't access it`);
+      }
 
-			const result = await slackClient.files.upload({
-				filepath,
-				title,
-				channels: room || process.env.SLACK_ROOM,
-				file: createReadStream(filepath),
-				icon_emoji: iconEmoji,
-				username: botName,
-			});
+      const result = await slackClient.files.upload({
+        filepath,
+        title,
+        channels: room || process.env.SLACK_ROOM,
+        file: createReadStream(filepath),
+        icon_emoji: iconEmoji,
+        username: botName,
+      });
 
-			debug(`File uploaded: ${result.file.id}`);
-			return result;
-		};
+      debug(`File uploaded: ${result.file.id}`);
+      return result;
+    };
 
-		return { sendMessage, uploadFile };
-	};
+    return { sendMessage, sendFile };
+  };
 
-	return { start };
+  return { start };
 };
